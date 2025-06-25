77383e177aac94a0780dbea1c7381870c73098a0
l-gu <laurent@telosys.org>
2025-06-25 11:24:02 +0200

    TextEditorVersion: just private constructor added

--- src/main/java/org/telosys/tools/editor/components/TextEditorVersion.java
+++ src/main/java/org/telosys/tools/editor/components/TextEditorVersion.java
@@ -16,6 +16,12 @@
 package org.telosys.tools.editor.components;
 
 public class TextEditorVersion {
+	
+	/**
+	 * Private constructor
+	 */
+	private TextEditorVersion() {
+	} 
 
 	public static final String VERSION = "1.0.0" ;
 }
